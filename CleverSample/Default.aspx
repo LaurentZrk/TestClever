123f06e70219e6686202dc55dbc3b1a7f531bdc5
LaurentZrk <laurent.rak@dojofactory.com>
2016-06-23 18:16:10 +0200

    First Commit
    Test For Clever

---
+++ CleverSample/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Bootstrap.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestForClever.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <br />
+    <br />
+    <p class="lead">Test for Clever</p>
+    <form runat="server">
+    
+    <asp:Label ID="label" runat="server"></asp:Label>
+        </form>
+</asp:Content>
---
+++ CleverSample/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestForClever
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public class Account
+{
+    public string Email { get; set; }
+    public bool Active { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public IList<string> Roles { get; set; }
+}
+    protected void Page_Load(object sender, EventArgs e)
+        {
+            string json = @"{
+   'Email': 'laurent.rak@dojo.fr',
+   'Active': true,
+   'CreatedDate': '2023-01-20T00:00:00Z',
+   'Roles': [
+     'User',
+     'Admin'
+   ]
+ }";
+            JToken token = JObject.Parse(json);
+
+            Account account =JsonConvert.DeserializeObject<Account>(json);
+
+            label.Text = account.Email;
+        }
+    }
+}
